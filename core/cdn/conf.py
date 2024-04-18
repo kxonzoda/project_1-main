d04d197a15ccbe3970f68757b1a3431d10479ebd
sayyUkhty <160317446+sayyUkhty@users.noreply.github.com>
2024-04-18 13:43:07 +0500

    Update conf.py

--- core/cdn/conf.py
+++ core/cdn/conf.py
@@ -5,8 +5,8 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_STORAGE_BUCKET_NAME = 'blogzine-space'
-AWS_ENDPOINT_URL = "https://fra1.digitaloceanspace.com"
-AWS_ENDPOINT = "fra1.digitaloceanspace.com"
+AWS_ENDPOINT_URL = "https://fra1.digitaloceanspaces.com"
+AWS_ENDPOINT = "fra1.digitaloceanspaces.com"
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
 }
